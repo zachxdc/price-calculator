9b031c499bdb399e817bf2a5af76b43697cf59c6
Zach <>
2021-11-17 01:18:26 +1100

    Fix 1 test

--- test/test_basePrice.py
+++ test/test_basePrice.py
@@ -1,45 +1,57 @@
 import sys
 sys.path.append(sys.path[0][:-4])
 import basePrice
+import unittest
 
-cart = {
-    "product-type": "hoodie",
-    "options": {
-        "size": "small",
-        "colour": "white",
+class TestCart(unittest.TestCase):
+
+    cart = {
+        "product-type": "hoodie",
+        "options": {
+            "size": "small",
+            "colour": "white",
+        }
     }
-}
 
-basePricesData = [{
-    "product-type": "hoodie",
-    "options": {
-        "colour": ["white", "dark"],
-        "size": ["small", "medium"]
+    basePricesData = [{
+        "product-type": "hoodie",
+        "options": {
+            "colour": ["white", "dark"],
+            "size": ["small", "medium"]
+        },
+        "base-price": 3800
+    }, {
+        "product-type": "sticker",
+        "options": {
+            "size": ["xl"]
+        },
+        "base-price": 1417
     },
-    "base-price": 3800
-}, {
-    "product-type": "sticker",
-    "options": {
-        "size": ["xl"]
-    },
-    "base-price": 1417
-},
-    {
-    "product-type": "leggings",
-    "options": {
-    },
-    "base-price": 5000
-}]
+        {
+        "product-type": "leggings",
+        "options": {
+        },
+        "base-price": 5000
+    }]
+
+    def test_getProductPrices(self):
+        self.assertEqual(basePrice.getProductPrices('hoodie', self.basePricesData) , [
+            {'basePrice': 3800, 'options': ["white", "dark","small", "medium"]}]) 
 
 
-def test_getProductPrices():
-    assert basePrice.getProductPrices('hoodie', basePricesData) == [
-        {'basePrice': 3800, 'options': ["white", "dark","small", "medium"]}]
+    def test_getCartOptions(self):
+        cart = {
+            "product-type": "hoodie",
+            "options": {
+                "size": "small",
+                "colour": "white",
+            }
+        }
+        self.assertEqual(basePrice.getCartOptions(cart) , ['small', 'white']) 
 
+    def test_getBasePrice(self):
+        print(basePrice.getBasePrice(self.cart, self.basePricesData))
+        self.assertEqual(basePrice.getBasePrice(self.cart, self.basePricesData) , 3800) 
 
-def test_getCartOptions():
-    assert basePrice.getCartOptions(cart) == ['small', 'white']
-
-
-def test_getBasePrice():
-    assert basePrice.getBasePrice(cart, basePricesData)
+if __name__ == '__main__':
+    unittest.main()
